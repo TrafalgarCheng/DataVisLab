1c2b4271ad4066f314f20f80c15c387417109c65
TrafalgarCheng <52690597+TrafalgarCheng@users.noreply.github.com>
2022-02-23 15:15:55 +0800

    Update Picture.java

--- src/main/java/com/ssm/demo/entity/Picture.java
+++ src/main/java/com/ssm/demo/entity/Picture.java
@@ -3,10 +3,6 @@ package com.ssm.demo.entity;
 import java.io.Serializable;
 import java.util.Date;
 
-/**
- * @author 13
- * @date 2018-07-18
- */
 public class Picture implements Serializable {
 
     //主键
